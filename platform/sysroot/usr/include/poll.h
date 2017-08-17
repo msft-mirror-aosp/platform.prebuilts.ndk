da8af43bcc8ba11134b6bc418d74f5249f46ddf4
Dan Albert <danalbert@google.com>
2017-08-17 09:24:31 -0700

    Update NDK platform prebuilts to build 4282922.
    Test: ndk/checkbuild.py && ndk/validate.py
    Bug: None
    Change-Id: Icbf81578653c7a9a3bd12776a7a4f34c371ce88f
    (cherry picked from commit 3fe4fc85ef689283634e66e673438c43dbb3e6a7)

--- platform/sysroot/usr/include/poll.h
+++ platform/sysroot/usr/include/poll.h
@@ -38,11 +38,10 @@ __BEGIN_DECLS
 
 typedef unsigned int nfds_t;
 
-int poll(struct pollfd*, nfds_t, int) __overloadable __RENAME_CLANG(poll);
+int poll(struct pollfd* __fds, nfds_t __count, int __timeout_ms) __overloadable __RENAME_CLANG(poll);
 
 #if __ANDROID_API__ >= 21
-int ppoll(struct pollfd*, nfds_t, const struct timespec*, const sigset_t*)
-        __overloadable __RENAME_CLANG(ppoll) __INTRODUCED_IN(21);
+int ppoll(struct pollfd* __fds, nfds_t __count, const struct timespec* __timeout, const sigset_t* __mask) __overloadable __RENAME_CLANG(ppoll) __INTRODUCED_IN(21);
 #endif /* __ANDROID_API__ >= 21 */
 
 
@@ -52,4 +51,4 @@ int ppoll(struct pollfd*, nfds_t, const struct timespec*, const sigset_t*)
 
 __END_DECLS
 
-#endif /* _POLL_H_ */
+#endif
