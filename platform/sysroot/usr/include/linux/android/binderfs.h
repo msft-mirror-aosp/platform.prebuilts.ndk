a5a0ece046d2ebbdfd11689aba79c6e9eaae4410
Dan Albert <danalbert@google.com>
2019-04-11 14:59:06 -0700

    Update NDK platform prebuilts to build 5460380.
    Test: ndk/checkbuild.py && ndk/run_tests.py
    Bug: None

--- platform/sysroot/usr/include/linux/android/binderfs.h
+++ platform/sysroot/usr/include/linux/android/binderfs.h
@@ -16,7 +16,16 @@
  ***
  ****************************************************************************
  ****************************************************************************/
-#ifndef __ASM_GENERIC_SHMPARAM_H
-#define __ASM_GENERIC_SHMPARAM_H
-#define SHMLBA PAGE_SIZE
+#ifndef _UAPI_LINUX_BINDERFS_H
+#define _UAPI_LINUX_BINDERFS_H
+#include <linux/android/binder.h>
+#include <linux/types.h>
+#include <linux/ioctl.h>
+#define BINDERFS_MAX_NAME 255
+struct binderfs_device {
+  char name[BINDERFS_MAX_NAME + 1];
+  __u32 major;
+  __u32 minor;
+};
+#define BINDER_CTL_ADD _IOWR('b', 1, struct binderfs_device)
 #endif
