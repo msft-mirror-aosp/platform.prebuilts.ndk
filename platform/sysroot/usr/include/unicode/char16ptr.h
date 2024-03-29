e616a2661b50e5bfcc5759751c2a4f7ab950c01a
Dan Albert <danalbert@google.com>
2024-03-29 22:51:56 +0000

    Update NDK platform prebuilts to build 11648676.
    Test: ndk/checkbuild.py && ndk/run_tests.py
    Bug: None
    Change-Id: Ia5b88aee825db9fea09b893f81659666ed6cfbe2

--- platform/sysroot/usr/include/unicode/char16ptr.h
+++ platform/sysroot/usr/include/unicode/char16ptr.h
@@ -41,7 +41,7 @@ U_NAMESPACE_BEGIN
  * char16_t * wrapper with implicit conversion from distinct but bit-compatible pointer types.
  * \xrefitem stable "Stable" "Stable List" ICU 59
  */
-class U_COMMON_API Char16Ptr U_FINAL {
+class U_COMMON_API Char16Ptr final {
 public:
     /**
      * Copies the pointer.
@@ -148,7 +148,7 @@ char16_t *Char16Ptr::get() const { return u_.cp; }
  * const char16_t * wrapper with implicit conversion from distinct but bit-compatible pointer types.
  * \xrefitem stable "Stable" "Stable List" ICU 59
  */
-class U_COMMON_API ConstChar16Ptr U_FINAL {
+class U_COMMON_API ConstChar16Ptr final {
 public:
     /**
      * Copies the pointer.
